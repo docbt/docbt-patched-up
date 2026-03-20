0bc9537abdef8a0a9b95c2c4a07323021e14fc53
docbt <docbt@users.noreply.github.com>
2026-03-20 16:57:58 +0100

    chore: initial project setup based on morphe-patches-template

---
+++ patches/build.gradle.kts
@@ -0,0 +1,45 @@
+group = "app.docbt.patched_up"
+
+patches {
+    about {
+        name = "docbt-patched-up"
+        description = "Revived and updated patches for the Morphe Patcher. First Release: Google News / Magazines"
+        source = "https://github.com/docbt/docbt-patched-up"
+        author = "docbt"
+        contact = "https://github.com/docbt/docbt-patched-up/issues"
+        website = "https://github.com/docbt/docbt-patched-up"
+        license = "GPLv3"
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.add("-Xcontext-parameters")
+    }
+}
+
+dependencies {
+    // Used by JsonGenerator.
+    implementation(libs.gson)
+}
+
+tasks {
+    register<JavaExec>("generatePatchesList") {
+        description = "Build patch with patch list"
+
+        dependsOn(build)
+
+        classpath = sourceSets["main"].runtimeClasspath
+        mainClass.set("app.morphe.util.PatchListGeneratorKt")
+    }
+    // Used by gradle-semantic-release-plugin.
+    publish {
+        dependsOn("generatePatchesList")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs = listOf("-Xcontext-receivers")
+    }
+}
